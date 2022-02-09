b01a03066f6001a44875f6ddf42aa15bc62d198f
Romero <rrcb@cin.ufpe.br>
2022-02-09 12:01:58 -0300

    build: add jquery to project

--- boilerplate/src/app/app.component.ts
+++ boilerplate/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+declare var $:JQueryStatic;
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
   title = 'boilerplate';
+  constructor() {
+    console.log("testing-jquery: " + $('.poke-infos'))
+    $('.poke-infos').hide();
+  }
 }
